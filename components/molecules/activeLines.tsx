56b2fd27ffa6a19a4e03dee8f24b823b89d23327
kozo yamagata <tune002@gmail.com>
2021-05-09 16:43:04 +0900

    Changes activeLines to be able to switch visibility

--- components/molecules/activeLines.tsx
+++ components/molecules/activeLines.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState, useCallback } from 'react'
 import styled from 'styled-components'
 import { useRecoilValue } from 'recoil'
 
@@ -40,12 +41,24 @@ type Props = {}
 export const ActiveLines: React.FC<Props> = (props) => {
   const activeCell = useRecoilValue(activeCellState)
   const size = useRecoilValue(sizeState)
+  const [isVisible, setVisible] = useState(false)
+
+  const handleMouseEnter = useCallback(() => {
+    setVisible(true)
+  }, [])
+
+  const handleMouseLeave = useCallback(() => {
+    setVisible(false)
+  }, [])
 
   return (
     <Container style={{ width: `${size.cols * 10}px` }}>
-      <YLine style={{ left: `${activeCell.col * 10}px` }} />
-      <XLine style={{ top: `${activeCell.row * 10}px` }} />
-      <GridContainer>
+      {isVisible && <YLine style={{ left: `${activeCell.col * 10}px` }} />}
+      {isVisible && <XLine style={{ top: `${activeCell.row * 10}px` }} />}
+      <GridContainer
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         {props.children}
       </GridContainer>
     </Container>
