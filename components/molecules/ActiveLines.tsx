eeb16af72bab6ca74d02c5539eea7fefeb8fb78e
kozo yamagata <tune002@gmail.com>
2021-05-10 21:25:13 +0900

    Tweaks ActiveLines

--- components/molecules/ActiveLines.tsx
+++ components/molecules/ActiveLines.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { useCallback } from 'react'
+import type { ReactNode } from 'react'
+import { useMemo, useCallback, memo } from 'react'
 import styled from 'styled-components'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 
@@ -38,13 +38,27 @@ const GridContainer = styled.div`
   z-index: 2;
 `
 
-type Props = {}
+type Props = {
+  children: ReactNode
+}
 
-export const ActiveLines: React.FC<Props> = (props) => {
+export const ActiveLines = memo((props: Props) => {
   const activeCell = useRecoilValue(activeCellState)
   const size = useRecoilValue(sizeState)
   const isVisible = useRecoilValue(activeLinesVisibilityState)
   const setActiveLinesVisibility = useSetRecoilState(activeLinesVisibilityState)
+
+  const containerStyle = useMemo(() => {
+    return { width: `${size.cols * variables.cell.size}px` }
+  }, [size.cols])
+
+  const yLineStyle = useMemo(() => {
+    return { left: `${activeCell.col * variables.cell.size}px` }
+  }, [activeCell.col])
+
+  const xLineStyle = useMemo(() => {
+    return { top: `${activeCell.row * variables.cell.size}px` }
+  }, [activeCell.row])
 
   const handleMouseEnter = useCallback(() => {
     setActiveLinesVisibility(true)
@@ -55,9 +69,9 @@ export const ActiveLines: React.FC<Props> = (props) => {
   }, [])
 
   return (
-    <Container style={{ width: `${size.cols * variables.cell.size}px` }}>
-      {isVisible && activeCell.col !== undefined && <YLine style={{ left: `${activeCell.col * variables.cell.size}px` }} />}
-      {isVisible && activeCell.row !== undefined && <XLine style={{ top: `${activeCell.row * variables.cell.size}px` }} />}
+    <Container style={containerStyle}>
+      {isVisible && activeCell.col !== undefined && <YLine style={yLineStyle} />}
+      {isVisible && activeCell.row !== undefined && <XLine style={xLineStyle} />}
       <GridContainer
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
@@ -66,4 +80,4 @@ export const ActiveLines: React.FC<Props> = (props) => {
       </GridContainer>
     </Container>
   )
-}
+})
